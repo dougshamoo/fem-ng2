7ede18db101e52cf7609e30ff73883b12ee01696
Doug Shamoo <doug.shamoo@gmail.com>
2016-05-04 21:25:15 -0700

    Complete step 1

--- app/common/state.service.ts
+++ app/common/state.service.ts
@@ -1,8 +1,14 @@
 import {Injectable} from 'angular2/core';
+import {Config} from './config.model';
 
 @Injectable()
 export class StateService {
 	private _message = 'Hello Message';
+  superGreeting: string = 'Hello, World!';
+  config: Config = {
+    name: 'Hello',
+    description: 'This is a description'
+  }
 
   getMessage(): string {
     return this._message;
