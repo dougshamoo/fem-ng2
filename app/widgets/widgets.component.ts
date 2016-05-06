483aa9572f9a608373c338319ae348bb4ab8fefc
Doug Shamoo <doug.shamoo@gmail.com>
2016-05-05 21:01:15 -0700

    Complete step 3

--- app/widgets/widgets.component.ts
+++ app/widgets/widgets.component.ts
@@ -3,10 +3,68 @@ import {Widget} from './widget.model.ts';
 
 @Component({
   selector: 'widgets',
-  templateUrl: 'app/widgets/widgets.component.html'
+  templateUrl: 'app/widgets/widgets.component.html',
+  styles: [`
+    button {
+      background-color: blue;
+      border: none;
+      border-radius: 5%;
+      outline: none;
+      padding: 10px;
+      min-width: 80px;
+      height: 50px;
+      color: white;
+      transition: transform .3s ease-in;
+    }
+
+    button[disabled] {
+      background-color: gray;
+      cursor: not-allowed;
+    }
+
+    .scroll {
+      height: 30px;
+      width: 300px;
+      overflow-y: scroll;
+    }
+  `]
 })
 export class WidgetsComponent {
   title: string = 'Widgets';
   body: string = 'This is the Widgets body';
   widgets: Widget[] = [];
+  interpolated: string = 'Click Me';
+  nums: number[] = [1, 2, 3];
+  person = {
+    name: 'Doug',
+    age: 29
+  };
+  isDisabled: boolean = false;
+  xpos: string = 'translateX(0px)';
+  count: number = 0;
+  inputVal: string = 'Enter text...';
+  show: boolean = true;
+  constructor() {
+    setInterval( () => {
+      this.transform();
+    }, 500);
+  };
+
+  transform() {
+    this.count += 5;
+    this.xpos = `translateX(${this.count}px)`;
+  }
+
+  onClick (event:Event) {
+    console.log(`${event.type} event activated!`);
+    this.isDisabled = !this.isDisabled;
+  }
+
+  onScroll () {
+    console.log('scrolling...')
+  }
+
+  toggleShow() {
+    this.show = !this.show;
+  }
 }
